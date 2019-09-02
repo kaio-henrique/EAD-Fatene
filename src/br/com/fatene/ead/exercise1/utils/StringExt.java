b6b2d7e64e7a8847538c08cebdf2bedc5e06a354
Kaio Henrique <kaiofreelancer@gmail.com>
2019-09-02 00:44:11 -0300

    Finish exercise one :mortar_board:

--- src/br/com/fatene/ead/exercise1/utils/StringExt.java
+++ src/br/com/fatene/ead/exercise1/utils/StringExt.java
@@ -1,11 +1,17 @@
 package br.com.fatene.ead.exercise1.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringExt {
+
+    private static final String PATTERN_REGEX = "(?i)[aeiou]";
 
     public void showValue(String input) {
         normal(input);
         reverse(input);
         hidden(input);
+        pLanguage(input);
     }
 
     private void normal(String input) {
@@ -18,11 +24,28 @@ public class StringExt {
     }
 
     private void hidden(String input) {
-        System.out.println("Hidden: " +input.replace(input, ""));
+        System.out.println("Hidden: " + input.replace(input, ""));
     }
 
     private void pLanguage(String input) {
-        // TODO: IMPLEMENTAR LÓGICA DA LINGUAGEM EM 'P'
+        StringBuilder sb = new StringBuilder();
+        Pattern tokenP = Pattern.compile(PATTERN_REGEX);
+
+        for (char s : input.toCharArray()) {
+            Matcher m = tokenP.matcher(String.valueOf(s));
+
+            if (m.find()) {
+                sb.append(s);
+            } else {
+                if (!" ".equals(String.valueOf(s))) {
+                    sb.append("P").append(s);
+                } else {
+                    sb.append(" ");
+                }
+            }
+        }
+
+        System.out.println("P Language: " + sb.toString());
     }
 
 }
